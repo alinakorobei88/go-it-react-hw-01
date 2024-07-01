22778c9518822623596639825781adca376e3c82
alinakorobei88 <alinakorobei@gmail.com>
2024-07-01 09:33:22 +0200

    change8

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,9 +3,9 @@ import styles from './FriendList.module.css';
 const FriendListItem = ({avatar, name, isOnLine}) => {
     return (
       <div className={styles.friend}>
-        <span className={`${styles.status} ${isOnLine ? styles.online : styles.offline}`}></span>
         <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
         <p className={styles.name}>{name}</p>
+        <span className={`${styles.status} ${isOnLine ? styles.online : styles.offline}`}></span>
       </div>
     );
 };
